a04b08eb892da50357c985e61dc1e553b5fb9ba5
DrSTaTTiK <drstattik2242@gmail.com>
2023-05-28 13:43:40 -0400

    rename

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "AutoClicker"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "AutoClicker"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "WalnutApp"
+include "AutoClicker"
